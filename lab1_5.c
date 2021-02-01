76289f11c9c7b15f30bb8528328440fa16092344
nared456 <78350512+nared456@users.noreply.github.com>
2021-02-01 18:54:40 +0700

    Edit code2 Max N_6320503041

--- lab1_5.c
+++ lab1_5.c
@@ -4,23 +4,19 @@ int main()
     int N,i,max,lo=0;
     scanf("%d",&N);
     int a[N];
-    for(i=1;i<=N;i++)
+    for(i=0;i<N;i++)
     {
         scanf("%d",&a[i]);
     }
-    max = a[1];
-    for(i=1;i<=N;i++)
+    max = a[0];
+    for(i=0;i<N;i++)
     {
         if(a[i]>max)
         {
             max = a[i];
             lo = i;
         }
-        if(a[i]==a[i+1])
-        {
-            lo = i;
-            break;
-        }
     }
-    printf("%d %d",lo,max);
+    printf("%d %d",lo+1,max);
+    return 0;
 }
