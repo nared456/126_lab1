6b8360a694e32a0b7f1fbaa50b74901ddc9e2dc5
nared456 <78350512+nared456@users.noreply.github.com>
2021-02-01 18:41:31 +0700

    Max N_6320503041

---
+++ lab1_5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int N,i,max,lo=0;
+    scanf("%d",&N);
+    int a[N];
+    for(i=1;i<=N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max = a[1];
+    for(i=1;i<=N;i++)
+    {
+        if(a[i]>max)
+        {
+            max = a[i];
+            lo = i;
+        }
+    }
+    printf("%d %d",lo,max);
+}
