fea7885236f9be2f1c865efd6151e7ac24877aa9
nared456 <78350512+nared456@users.noreply.github.com>
2021-02-01 19:36:49 +0700

    a.m. & p.m.

--- lab1_7.c
+++ lab1_7.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main()
+{
+    float time;
+    int a;
+    scanf("%f",&time);
+    a = (int)time;
+    printf("%.2f ",time);
+    if(a>=0 && a<=11)
+    {
+        printf("a.m.");
+    }
+    else if(a>=12 && a<=23)
+    {
+        printf("p.m.");
+    }
+    return 0;
+}
